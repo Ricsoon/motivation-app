4a1567128b9dd718d1ab1c94a6ede19f9b8724ff
Ricsoon <painonex2@gmail.com>
2022-08-01 20:26:24 -0300

    Criação da página inicial e seu layout

---
+++ app/src/main/java/com/horizoncorp/motivationapp/UserActivity.kt
@@ -0,0 +1,26 @@
+package com.horizoncorp.motivationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.horizoncorp.motivationapp.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+
+        }
+    }
+}

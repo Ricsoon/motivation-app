27f084d5ccde62610177c0c2ca24150b2004b8e1
Ricsoon <painonex2@gmail.com>
2022-08-01 20:37:46 -0300

    Mudança de telas após a atividade

--- app/src/main/java/com/horizoncorp/motivationapp/UserActivity.kt
+++ app/src/main/java/com/horizoncorp/motivationapp/UserActivity.kt
@@ -1,8 +1,10 @@
 package com.horizoncorp.motivationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.horizoncorp.motivationapp.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,8 +21,19 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_save){
-
+        if (v.id == R.id.button_save) {
+            handleSave()
         }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
